245c1a393e1b18b8b05d73bd88298101ffa2ae8f
Briotar <vladimirklevtsovmain@gmail.com>
2021-05-19 14:53:07 +0300

    Task 2

---
+++ task2/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> purchaseAmount = new Queue<int>();
+            int sum = 0;
+
+            purchaseAmount.Enqueue(1);
+            purchaseAmount.Enqueue(3);
+            purchaseAmount.Enqueue(5);
+            purchaseAmount.Enqueue(7);
+            purchaseAmount.Enqueue(9);
+            purchaseAmount.Enqueue(11);
+
+            while (purchaseAmount.Count != 0)
+            {
+                sum += purchaseAmount.Peek();
+                purchaseAmount.Dequeue();
+                Console.WriteLine($"Your account = {sum}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+    }
+}
