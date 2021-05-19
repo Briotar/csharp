4345aea4051091dde09d4fcfcc7dda08edef9f33
Briotar <vladimirklevtsovmain@gmail.com>
2021-05-19 15:49:28 +0300

    Task 3

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -25,7 +25,6 @@ namespace task2
                 Console.ReadKey();
                 Console.Clear();
             }
-
         }
     }
 }
---
+++ task3/task3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            string userInput = "";
+            int newNumber;
+            int sum = 0;
+
+            while (true)
+            {
+                Console.Write("Enter number - ");
+                userInput = Console.ReadLine();
+
+                if (userInput == "sum")
+                {
+                    for (int i=0; i < numbers.Count; i++)
+                    {
+                        sum += numbers[i];
+                    }
+                    Console.WriteLine($"Sum - {sum}");
+                }
+                else if(userInput == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    newNumber = ConvertToInt(userInput);
+                    numbers.Add(newNumber);
+                }
+            }
+        }
+
+        static int ConvertToInt(string userInput)
+        {
+            int number = 0;
+            bool successfulConvert = false;
+
+            while (successfulConvert == false)
+            {
+                successfulConvert = Int32.TryParse(userInput, out number);
+                if (successfulConvert)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("It's not a number!");
+                }
+            }
+            return 0;
+        }
+
+    }
+}
