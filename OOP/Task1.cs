bc2069882039c6b5664d126cd07d20214d44331d
Briotar <vladimirklevtsovmain@gmail.com>
2021-05-26 17:45:55 +0300

    Task 1 in OOP

---
+++ OOP/Task1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(5, 5);
+
+            player.ShowStats();
+            
+        }
+    }
+
+    class Player
+    {
+        private int _x;
+        private int _y;
+
+        public Player(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine($"Координата X - {_x}, координата Y - {_y}");
+        }
+    }
+
+}
